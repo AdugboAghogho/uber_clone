5d71218740a5a5be2dd467cf29fec1bf14985679
adugbo aghogho <adugboaghogho@gmail.com>
2024-10-07 16:04:52 +0100

    cxx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
   content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
 }
-
