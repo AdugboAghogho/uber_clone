cbd94bd2fb1c8ae2b87f27e8770106fba05fff4f
adugbo aghogho <adugboaghogho@gmail.com>
2024-10-15 17:28:37 +0100

    .k

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,39 @@
-// import { StatusBar, Text,  } from 'react-native';
-// import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-
-// export default function HomeScreen() {
-//   return (
-//     <SafeAreaView className="flex-1 items-center justify-center bg-black">
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </SafeAreaView>
-//   );
-// }
-
-import { StatusBar } from 'expo-status-bar'; // If using Expo
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+    <SafeAreaView className="flex-1 items-center justify-center bg-black">
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'black',
-  },
-  text: {
-    color: 'white',
-  },
-});
+// import { StatusBar } from 'expo-status-bar'; // If using Expo
+// import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+// export default function HomeScreen() {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: 'black',
+//   },
+//   text: {
+//     color: 'white',
+//   },
+// });
+
+
 
