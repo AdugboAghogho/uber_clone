00cad7b9668cd9848b5a1ffe7aa67471020ddc2f
adugbo aghogho <adugboaghogho@gmail.com>
2024-11-16 16:23:18 +0100

    x

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { ActivityIndicator, Text, View } from "react-native";
-import MapView, { Marker, PROVIDER_DEFAULT } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 
 import { icons } from "@/constants";
@@ -16,82 +16,84 @@ import { Driver, MarkerData } from "@/types/type";
 const directionsAPI = process.env.EXPO_PUBLIC_DIRECTIONS_API_KEY;
 
 const Map = () => {
-  const {
-    userLongitude,
-    userLatitude,
-    destinationLatitude,
-    destinationLongitude,
-  } = useLocationStore();
-  const { selectedDriver, setDrivers } = useDriverStore();
+  // const {
+  //   userLongitude,
+  //   userLatitude,
+  //   destinationLatitude,
+  //   destinationLongitude,
+  // } = useLocationStore();
+  // const { selectedDriver, setDrivers } = useDriverStore();
 
-  const { data: drivers, loading, error } = useFetch<Driver[]>("/(api)/driver");
-  const [markers, setMarkers] = useState<MarkerData[]>([]);
+  // const { data: drivers, loading, error } = useFetch<Driver[]>("/(api)/driver");
+  // const [markers, setMarkers] = useState<MarkerData[]>([]);
 
-  useEffect(() => {
-    if (Array.isArray(drivers)) {
-      if (!userLatitude || !userLongitude) return;
+  // useEffect(() => {
+  //   if (Array.isArray(drivers)) {
+  //     if (!userLatitude || !userLongitude) return;
 
-      const newMarkers = generateMarkersFromData({
-        data: drivers,
-        userLatitude,
-        userLongitude,
-      });
+  //     const newMarkers = generateMarkersFromData({
+  //       data: drivers,
+  //       userLatitude,
+  //       userLongitude,
+  //     });
 
-      setMarkers(newMarkers);
-    }
-  }, [drivers, userLatitude, userLongitude]);
+  //     setMarkers(newMarkers);
+  //   }
+  // }, [drivers, userLatitude, userLongitude]);
 
-  useEffect(() => {
-    if (
-      markers.length > 0 &&
-      destinationLatitude !== undefined &&
-      destinationLongitude !== undefined
-    ) {
-      calculateDriverTimes({
-        markers,
-        userLatitude,
-        userLongitude,
-        destinationLatitude,
-        destinationLongitude,
-      }).then((drivers) => {
-        setDrivers(drivers as MarkerData[]);
-      });
-    }
-  }, [markers, destinationLatitude, destinationLongitude]);
+  // useEffect(() => {
+  //   if (
+  //     markers.length > 0 &&
+  //     destinationLatitude !== undefined &&
+  //     destinationLongitude !== undefined
+  //   ) {
+  //     calculateDriverTimes({
+  //       markers,
+  //       userLatitude,
+  //       userLongitude,
+  //       destinationLatitude,
+  //       destinationLongitude,
+  //     }).then((drivers) => {
+  //       setDrivers(drivers as MarkerData[]);
+  //     });
+  //   }
+  // }, [markers, destinationLatitude, destinationLongitude]);
 
-  const region = calculateRegion({
-    userLatitude,
-    userLongitude,
-    destinationLatitude,
-    destinationLongitude,
-  });
+  // const region = calculateRegion({
+  //   userLatitude,
+  //   userLongitude,
+  //   destinationLatitude,
+  //   destinationLongitude,
+  // });
 
-  if (loading || (!userLatitude && !userLongitude))
-    return (
-      <View className="flex justify-between items-center w-full">
-        <ActivityIndicator size="small" color="#000" />
-      </View>
-    );
+  // if (loading || (!userLatitude && !userLongitude)) {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", width: "100%" }}>
+  //       <ActivityIndicator size="small" color="#000" />
+  //     </View>
+  //   );
+  // }
 
-  if (error)
-    return (
-      <View className="flex justify-between items-center w-full">
-        <Text>Error: {error}</Text>
-      </View>
-    );
+  // if (error) {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", width: "100%" }}>
+  //       <Text>Error: {error}</Text>
+  //     </View>
+  //   );
+  // }
 
   return (
     <MapView
-      provider={PROVIDER_DEFAULT}
-      className="w-full h-full rounded-2xl"
+      provider={PROVIDER_GOOGLE}
+      style={{ width: "100%", height: "100%", borderRadius: 12 }}
       tintColor="black"
       mapType="mutedStandard"
       showsPointsOfInterest={false}
-      initialRegion={region}
-      showsUserLocation={true}
+      // initialRegion={region}
+      // showsUserLocation={true}
       userInterfaceStyle="light"
     >
-      {markers.map((marker, index) => (
+      {/* {markers.map((marker) => (
         <Marker
           key={marker.id}
           coordinate={{
@@ -130,7 +132,8 @@ const Map = () => {
             strokeWidth={2}
           />
         </>
-      )}
+      )} */}
+      <Text>MAP</Text>
     </MapView>
   );
 };
